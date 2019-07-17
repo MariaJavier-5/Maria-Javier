f2675cb23f6a1be4a272d92e7335baf648c00936
MariaJavier-5 <53014020+MariaJavier-5@users.noreply.github.com>
2019-07-17 09:58:52 -0700

    Create website.js

---
+++ website.js
@@ -0,0 +1,16 @@
+// function myFunction (){
+//   alert("Hi")
+// }
+function load(){
+  document.getElementById("expand").style.visibility = "hidden";
+  document.getElementById("RF").style.visibility = "hidden";
+}
+
+
+function expandFunc(){
+  document.getElementById("expand").style.visibility = 'visible';
+}
+
+function expandRF(){
+  document.getElementById("RF").style.visibility = 'visible';
+}
